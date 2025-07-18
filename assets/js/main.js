ac94e841205383fbc66bf8f0a08c8cb6c1d4e2bf
Mishraaa-Anant <your_email@example.com>
2025-07-18 21:13:04 +0530

    Initial gym website push

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+/*=============== SHOW MENU ===============*/
+const toggleBtn = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+const closeBtn = document.getElementById("nav-close");
+// show menu
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+// hidden menu
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(".nav__link");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+});
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+/*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
+
+const calculateBmi = (e) => {
+  e.preventDefault();
+
+  // Check if the fields have a value
+  if (calculateCm.value === "" || calculateKg.value === "") {
+    // Add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
+
+    // Show message
+    calculateMessage.textContent = "Fill in the Height and Weight 🫤";
+
+    // Remove message three seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    // BMI Formula
+    const cm = calculateCm.value / 100,
+      kg = calculateKg.value,
+      bmi = Math.round(kg / (cm * cm));
+
+    // Show your health status
+    if (bmi < 18.5) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😞`;
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 🥳`;
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight 🫤`;
+    }
+
+    // To clear the input field
+    calculateCm.value = "";
+    calculateKg.value = "";
+
+    // Remove message after 5 seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 5000);
+  }
+};
+
+calculateForm.addEventListener("submit", calculateBmi);
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(`.home__data, .footer__container, .footer__group`);
+sr.reveal(`.home__img`, { delay: 700, origin: "bottom" });
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 });
+sr.reveal(`.choose__img, .calculate__content`, { origin: "left" });
+sr.reveal(`.choose__content, .calculate__img`, { origin: "right" });
+
+
+  function sendToWhatsApp(form) {
+    const name = form.name.value;
+    const interest = form.interest.value;
+    const phoneNumber = "917028159983"; // Your WhatsApp number
+
+    const message = `Hi, my name is ${name}. I’m interested in the ${interest} plan. Please share more details.`;
+
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+    return false; // Prevent form from refreshing
+  }
